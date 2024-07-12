cd6d1dfdd6e3a2d5428d0d8c9641ec7508b75829
Marc Eid <marc.mn.eid@gmail.com>
2024-07-12 14:06:46 -0400

    added data retrieval and ingestion

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,4 +5,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const data = require('./data_management/retrieve_and_ingest_data');
+
+app.use('/ingest_data', data);
+
 app.listen(PORT, () => { console.log(`Server is listening at http://localhost:${PORT}`) });
